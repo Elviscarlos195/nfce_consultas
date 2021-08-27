c287a3e6d3ff8f042e0e1a50aa947efe39366da5
elviscarlos-marques <elviscarlos.vix@hotmail.com>
2021-08-27 13:42:00 -0300

    envio

---
+++ .next/static/JvD9KESYjDPGLLCoN4R1v/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/eb291d7de33c874d5a88.css","static/chunks/pages/index-d33e1323a91bce3b15c3.js"],"/_error":["static/chunks/pages/_error-67fba61e1425a3723b83.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
